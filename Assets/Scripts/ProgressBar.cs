425d259016b627e189ed53845eaa79db64349aea
Jing <jtan1993@gmail.com>
2019-01-13 22:23:27 +0000

    animated ui bars

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour {
+    Image image;
+    [SerializeField] private float fillSpeed = 0.1f; //the fill amount increments per second.
+
+    void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	void Update () {
+        image.fillAmount += Time.deltaTime * fillSpeed;
+
+    }
+}
