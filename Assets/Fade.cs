db57033670a731ce8b7410d5ecd1b751e4d183be
Jing <jtan1993@gmail.com>
2019-01-14 23:34:30 +0000

    added fade

---
+++ Assets/Fade.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour
+{
+
+    Animator animator;
+
+    void Start()
+    {
+
+        //assign a reference to the Animator component of the same game object
+        animator = GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //set the "Fade" trigger on the animator every time, player presses Space
+            animator.SetTrigger("Fade");
+        }
+
+    }
+}
